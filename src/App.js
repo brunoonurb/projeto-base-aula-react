7754e96fa462e4e7e800a2e60c0ac75c93b9940f
Bruno da Silva Pedroso <bruno.pedroso@n202640579.local>
2026-04-20 19:03:48 -0300

    ajuste APP

--- src/App.js
+++ src/App.js
@@ -7,19 +7,6 @@ import { PrimeiroComponente } from "./componentes/PrimeiroComponente";
 function App() {
   return (
     <div className="App">
-      <PrimeiroComponente sobrenome="Pedroso" idade="20" />
-      <hr />
-      <PrimeiroComponente 
-        sobrenome="Silva" 
-        idade="30" 
-        altura="1.75"
-      >
-        Este é o conteúdo do props.children
-      </PrimeiroComponente>
-      <hr />
-      <PrimeiroComponente sobrenome="Oliveira">
-        esse ehsegundo conteúdo
-      </PrimeiroComponente>
       Helo World!
     </div>
   );
