e8665d54913417eb2f7961ebb37fdc4c9e820269
Bruno da Silva Pedroso <bruno.pedroso@n202640579.local>
2026-04-27 20:29:11 -0300

    toast

--- src/App.js
+++ src/App.js
@@ -4,41 +4,15 @@ import "font-awesome/css/font-awesome.css";
 import "react-toastify/dist/ReactToastify.css";
 
 import { RouteApp } from "./route";
-
-
+import { ToastContainer } from "react-toastify";
 
 function App() {
-
   return (
 
-<>
-<RouteApp/>
-</>
-
-    // <div className="App">
-
-
-
-    //   <Nome />
-
-    //   <Sobrenome />
-
-
-    //   <h1>Habilidades</h1>
-
-    //   <Habilidade> HTML </Habilidade>
-    //   <Habilidade> CSS </Habilidade>
-    //   <Habilidade> JS </Habilidade>
-    //   <Habilidade> REACT </Habilidade>
-    //   <Habilidade> NODE </Habilidade>
-    //   <Habilidade> GIT </Habilidade>
-
-
-    //   <h1>Experiencias</h1>
-
-    //   <Experiencia cargo="Estagiario" empresa="Harve" periodo="2023-momento" />
-
-    // </div>
+    <>
+    <ToastContainer />
+      <RouteApp />
+    </>
   );
 }
 
